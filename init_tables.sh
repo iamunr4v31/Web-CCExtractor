346f914dc6d13b309296237c5bd60fe44168e279
Ashwin S <iunravel@LAPTOP-OGFLB4R0>
2022-10-09 16:39:25 +0530

    added nav

--- init_tables.sh
+++ init_tables.sh
@@ -1,3 +1,3 @@
-AWS_PAGER="" aws dynamodb create-table --cli-input-json file://captions.json --endpoint-url=http://localhost:4566
-AWS_PAGER="" aws dynamodb create-table --cli-input-json file://users.json --endpoint-url=http://localhost:4566
-aws s3 mb s3://mybucket --endpoint-url=http://localhost:4566
+AWS_PAGER="" aws dynamodb create-table --cli-input-json file://captions.json --endpoint-url=http://34.93.83.118:4566
+AWS_PAGER="" aws dynamodb create-table --cli-input-json file://users.json --endpoint-url=http://34.93.83.118:4566
+aws s3 mb s3://mybucket --endpoint-url=http://34.93.83.118:4566
